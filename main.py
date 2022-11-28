9be0b090d93e0bb2f6db1ec62c2e271f36385200
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-11-28 17:16:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ class Detector(AddOn):
         
         for document in self.get_documents():
             for page in range(1,document.pages+1):
-                url = (document.asset_url + f"documents/{document.id}/pages/" + f"{document.slug}-p{page}.position.json")
-                resp = self.client.get(url)
+                url = (document.asset_url + f"documents/23303872/pages/" + f"{document.slug}-p{page}.position.json")
+                resp = self.client.get(url, full_url=True)
                 positions = resp.json()
                 print(positions[:3])
                 for info in positions:
