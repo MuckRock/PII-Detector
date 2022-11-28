2eebbb4f6d71fee9d051cb2a1eea54a3812dc5a6
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-11-28 09:01:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import commonregex as CommonRegex
 
 class Detector(AddOn):
     def main(self):
+        detect_address = self.data['address']
+        detect_phone = self.data['phone']
+        detect_email = self.data['email']
+        
         for document in self.get_documents():
             for page in range(1,document.pages+1):
                 text=document.get_page_text(page)
@@ -14,6 +18,7 @@ class Detector(AddOn):
                 email_list = CommonRegex.emails(text)
                 phone_list = CommonRegex.phones(text)
                 iban_list = CommonRegex.iban_numbers(text)
+                address_list = CommonRegex.street_addresses(text)
                 
                 for ssn in ssn_list:
                     document.annotations.create("SSN Found", (page-1), content=ssn)
@@ -21,10 +26,16 @@ class Detector(AddOn):
                     document.annotations.create("CC Found", (page-1), content=cc)
                 for iban in iban_list:
                     document.annotations.create("IBAN # Found", (page-1), content=iban)
-                for email in email_list:
-                    document.annotations.create("Email Found", (page-1), content=email)
-                for phone in phone_list:
-                    document.annotations.create("Phone # Found", (page-1), content=phone)
+                
+                if detect_email is True:
+                    for email in email_list:
+                        document.annotations.create("Email Found", (page-1), content=email)
+                if detect_phone is True:
+                    for phone in phone_list:
+                        document.annotations.create("Phone # Found", (page-1), content=phone)
+                if detect_email is True:
+                    for address in address_list:
+                        document.annotations.create("Address Found", (page-1), content=address)
                 
 if __name__ == "__main__":
     Detector().main()
