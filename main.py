3379f0568d5a505bbee70dadc9ff8b08622a07d2
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-11-29 20:17:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,16 +28,16 @@ class Detector(AddOn):
                 resp = requests.get(url, timeout=10)
                 positions = resp.json()
                 
-                for ssn in ssn_list:
-                    for info in positions:
-                        if ssn in info['text']:
-                            document.annotations.create(f"SSN found",page-1,x1=info["x1"],y1=info["y1"],x2=info["x2"],y2=info["y2"])
-                
                 for cc in cc_list:
                     document.annotations.create("CC Found", (page-1), content=f"Last four digits: {cc[-4:]}")
                 
                 for iban in iban_list:
                     document.annotations.create("IBAN # Found", (page-1), content=f"Last two digits: {iban[-2:]}")
+
+                for ssn in ssn_list:
+                    for info in positions:
+                        if ssn in info['text']:
+                            document.annotations.create(f"SSN found",page-1,x1=info["x1"],y1=info["y1"],x2=info["x2"],y2=info["y2"])
 
                 if detect_email is True:
                     email_list = CR.emails(text)
@@ -59,37 +59,7 @@ class Detector(AddOn):
                     address_list = CR.street_addresses(text)
                     for address in address_list:
                         document.annotations.create("Address found on this page", (page-1), content=address)
-                                
-                """text=document.get_page_text(page)
-                ssn_list = CommonRegex.ssn_numbers(text)
-                cc_list = CommonRegex.credit_cards(text)
-                iban_list = CommonRegex.iban_numbers(text)
-                for ssn in ssn_list:
-                    document.annotations.create("SSN Found", (page-1), content=ssn)
-                    send_ssn_mail=True
-                for cc in cc_list:
-                    document.annotations.create("CC Found", (page-1), content=cc)
-                    send_cc_mail=True
-                for iban in iban_list:
-                    document.annotations.create("IBAN # Found", (page-1), content=iban)
-                    send_iban_mail=True
-           
-                if detect_email is True:
-                    email_list = CommonRegex.emails(text)
-                    for email in email_list:
-                        document.annotations.create("Email Found", (page-1), content=email)
-                if detect_phone is True:
-                    phone_list = CommonRegex.phones(text)
-                    phone_with_ext_list = CommonRegex.phones_with_exts(text)            
-                    for phone in phone_list:
-                        document.annotations.create("Phone # Found", (page-1), content=phone)
-                    for phone_with_ext in phone_with_ext_list:
-                        document.annotations.create("Phone # Found", (page-1), content=phone_with_ext)
-                if detect_address is True:
-                    address_list = CommonRegex.street_addresses(text)
-                    for address in address_list:
-                        document.annotations.create("Address Found", (page-1), content=address)
-        
+        """                        
         if send_cc_mail is True:
             self.send_mail("Sensitive PII Detected", f"Credit Card # found in {document.canonical_url}")
         if send_ssn_mail is True:
