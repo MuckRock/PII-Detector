6c656aed234876ff6aa4c4fac6d61f1ad1f08425
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-11-29 21:11:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Detector(AddOn):
         detect_address = self.data.get('address')
         detect_phone = self.data.get('phone')
         detect_email = self.data.get('email')
+        detect_PII = False
         alert = self.data.get('alert')
         
         for document in self.get_documents():
@@ -33,9 +34,11 @@ class Detector(AddOn):
                 """
                 for cc in cc_list:
                     document.annotations.create("CC Found", (page-1), content=f"Last four digits: {cc[-4:]}")
+                    detect_PII = True
                 
                 for iban in iban_list:
                     document.annotations.create("IBAN # Found", (page-1), content=f"Last two digits: {iban[-2:]}")
+                    detect_PII = True
 
                 if detect_email is True:
                     email_list = CR.emails(text)
@@ -45,6 +48,7 @@ class Detector(AddOn):
                             print(type(info))
                             if email in info['text']:
                                 document.annotations.create(f"Email found",page-1,x1=info["x1"],y1=info["y1"],x2=info["x2"],y2=info["y2"])
+                                detect_PII = True
 
                 if detect_phone is True:
                     phone_list = CR.phones(text) + CR.phones_with_exts(text)
@@ -53,14 +57,16 @@ class Detector(AddOn):
                         for info in positions:
                             if phone in info['text']:
                                 document.annotations.create(f"Phone # found",page-1,x1=info["x1"],y1=info["y1"],x2=info["x2"],y2=info["y2"])
+                                detect_PII = True
                 
                 if detect_address is True:
                     address_list = CR.street_addresses(text)
                     for address in address_list:
                         document.annotations.create("Address found on this page", (page-1), content=address)
+                        detect_PII = True
                      
-        if alert is True:
-            self.send_mail("PII Detected", f"PII was found in {document.canonical_url}")
+        if alert and detect_PII is True:
+            self.send_mail("PII Detected", f"Personally identifying information was found in {document.canonical_url} please open the document to view more detail.")
        
 if __name__ == "__main__":
     Detector().main()
